7aceb86a5e4fe6837455303553260b18911fc347
Collins Adom Baffour <collins.baffour@amalitech.org>
2021-06-25 14:41:47 +0000

    Updated

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -1,4 +1,5 @@
 const addMovieModal = document.getElementById('add-modal');
+const backdrop = document.querySelector('.backdrop')
 const desc = document.getElementById('desc');
 const addbtn = document.getElementById('addBtn');
 const cancelBtn = addMovieModal.querySelector('.btn--passive')
@@ -9,8 +10,48 @@ const updatestockBtn = document.getElementById('stocks');
 const detailsBtn = document.getElementById('details');
 const detailStockBtn = document.getElementById('detailStock');
 const updateStckModal = document.getElementById('add-stock')
-window.addEventListener('load', initStorage);
+const editor = desc.getElementsByClassName('edit');
+const sel = document.querySelector('.categorySelect #cat')
+const del = desc.getElementsByClassName('del')
+const canUp = updatedModal.querySelector('.btn--passive');
+const canUps = updateStckModal.querySelector('.btn--passive');
+const qtyC = document.getElementsByClassName('qtyCell')
 
+window.addEventListener('load', () => {
+    initStorage();
+
+    let editbtnArray = [...editor]
+    editbtnArray.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            let id = e.target.closest("tr").rowIndex;
+            editDet()
+            showItems(id);
+            detailsBtn.addEventListener('click', () => {
+                editItem(id)
+                closeModalUp()
+            })
+        });
+    })
+    let delBtnArray = [...del];
+    delBtnArray.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            let id = e.target.closest("tr").rowIndex;
+            e.target.closest("tr").remove();
+            updateLocalStorage(id);
+        });
+    });
+    let quantA = [...qtyC]
+    quantA.forEach(cel => {
+        console.log(cel.parentElement)
+        let celCount = parseInt(cel.innerHTML)
+        if (celCount == 0) {
+            cel.classList.add('outOfStock')
+        } else if (celCount < 20) {
+            cel.classList.add('allmostOutOfStock')
+        } else { cel.classList.add('inStock') }
+    })
+});
+// window.addEventListener('load', dasvalues);
 
 //item Array
 let itemsArray = [];
@@ -23,31 +64,38 @@ function initStorage() {
 
             prepareTable(itemsArray[i].prodName, itemsArray[i].deSc, itemsArray[i].quantic, itemsArray[i].categ);
 
-
-
         }
+
         // for (var j = 0; j < localStorage.length; j++) {
         //     console.log(localStorage.getItem(localStorage.key(j)));
         // }
     }
 }
 
+function dasvalues() {
+    localStorage.length
+}
+const editDet = () => {
 
-
+    backdrop.classList.add('visible')
+    updatedModal.classList.add('visible');
+}
 
 const updateHandlerHan = () => {
     updatedModal.classList.add('visible');
+    backdrop.classList.add('visible')
 }
 
 const updateHandlerStock = () => {
     updateStckModal.classList.add('visible');
+    backdrop.classList.add('visible')
 }
 
 
 //show addItem window
 const showAddItem = () => {
     addMovieModal.classList.add('visible');
-
+    backdrop.classList.add('visible')
 }
 const addItemList = () => {
     insertNewItem();
@@ -58,10 +106,24 @@ const addItemList = () => {
 const updateItemList = () => {
     console.log("something has happended")
 }
+
+
+
 const closeModal = () => {
-    addMovieModal.classList.remove('visible');
-    resetForm;
+    addMovieModal.classList.remove('visible')
+    backdrop.classList.remove('visible')
+
 }
+const closeModalUp = () => {
+    updatedModal.classList.remove('visible')
+    backdrop.classList.remove('visible')
+}
+const closeModalUps = () => {
+    updateStckModal.classList.remove('visible')
+    backdrop.classList.remove('visible')
+}
+
+
 
 function insertNewItem() {
 
@@ -105,9 +167,11 @@ function prepareTable(prodName, deSc, quantic, categ) {
     cell5 = newRow.insertCell(4)
     cell5.innerHTML = `<button class ="edit">Edit</button>
                         <button class="del">Delete</button>`
-
+    cell4.classList.add('qtyCell')
 }
-// <i class="fas fa-trash"></i>
+
+
+
 function resetForm() {
     prodName.value = "";
     deSc.value = "";
@@ -115,13 +179,60 @@ function resetForm() {
     categ.value = "";
 }
 
-// function edit(td) {
-//     selectedRow = td.parentElement.parentElement;
-//     document.getElementById("")
-// }
+
 addbtn.addEventListener('click', showAddItem);
 cancelBtn.addEventListener('click', closeModal);
 addItemBtn.addEventListener('click', addItemList);
-updateBtn.addEventListener('click', updateHandlerHan);
+// updateBtn.addEventListener('click', updateHandlerHan);
 detailsBtn.addEventListener('click', updateItemList);
-updatestockBtn.addEventListener('click', updateHandlerStock);
+// updatestockBtn.addEventListener('click', updateHandlerStock);
+canUp.addEventListener('click', closeModalUp)
+canUps.addEventListener('click', closeModalUps)
+
+//------------------------------------------------------------------------------
+const editItem = (id) => {
+    let prodName = document.getElementById("pname")
+    let deSc = document.getElementById("pdescript")
+    let quantic = document.getElementById("pquant")
+    let categ = document.getElementById("pcat")
+
+    let products = JSON.parse(localStorage.getItem("itemsRecord"));
+    products.forEach((product, index) => {
+        if (index == id - 1) {
+            product.prodName = prodName.value;
+            product.desc = deSc.value;
+            product.quantic = quantic.value;
+            product.categ = categ.value;
+        }
+    });
+    localStorage.setItem("itemsRecord", JSON.stringify(products));
+    location.reload();
+};
+
+const showItems = (id) => {
+    let prodName = document.getElementById("pname")
+    let deSc = document.getElementById("pdescript")
+    let quantic = document.getElementById("pquant")
+    let categ = document.getElementById("pcat")
+
+    let products = JSON.parse(localStorage.getItem("itemsRecord"));
+    console.log(products)
+    products.forEach((product, index) => {
+        if (index == id - 1) {
+            prodName.value = product.prodName;
+            deSc.value = product.deSc;
+            quantic.value = product.quantic;
+            categ.value = product.categ;
+        }
+    });
+};
+
+const updateLocalStorage = (id) => {
+    let product = JSON.parse(localStorage.getItem("itemsRecord"));
+    for (let i = 0; i < product.length; i++) {
+        if (i == id - 1) {
+            product.splice(i, 1);
+        }
+        localStorage.setItem("itemsRecord", JSON.stringify(product));
+    }
+};
